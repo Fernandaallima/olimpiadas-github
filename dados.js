f8ef25e54e750b1f35e4dbe19dce9a086fbc80a2
Fernandaallima <fernandaalencardelima1@gmail.com>
2024-09-06 21:12:54 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,24 @@
+let dados = [
+{
+    titulo : "Rayssa Leal",
+    esporte : "Skate",
+    descricao : "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos [idade], ela já é considerada uma das maiores skatistas de sua geração.",
+    link : "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+},
+    
+{
+    titulo : "Rebeca Andrade",
+    esporte: "Ginástica Artística",
+    descricao: "Rebeca Andrade é uma ginasta brasileira que encantou o mundo com suas acrobacias e beleza. Aos [idade], ela é considerada uma das maiores promessas da ginástica mundial.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+},
+    
+{
+    titulo : "Beatriz Souza",
+    esporte: "Judô",
+    descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e força. Com apenas [idade], ela já conquistou diversas medalhas em competições nacionais e internacionais.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+}
+]
+
+
